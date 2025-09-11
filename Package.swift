50030d1e22c7c99a8e36a9c782305c21976048b9
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-09-11 15:27:46 +0200

    refactor: consolidate dependencies through TypesFoundation
    Replace individual type and utility dependencies with unified TypesFoundation
    package. This simplifies dependency management by centralizing common types
    and utilities (Tagged, URLRouting, EmailAddress, Domain, etc.) into a single
    upstream package while maintaining the same public API through re-exports.

--- Package.swift
+++ Package.swift
@@ -13,29 +13,16 @@ extension Target.Dependency {
 }
 
 extension Target.Dependency {
-    static var builders: Self { .product(name: "Builders", package: "swift-builders") }
+    static var typesFoundation: Self { .product(name: "TypesFoundation", package: "swift-types-foundation") }
     static var urlRequestHandler: Self { .product(name: "URLRequestHandler", package: "swift-urlrequest-handler") }
     static var loggingExtras: Self { .product(name: "LoggingExtras", package: "swift-logging-extras") }
-    static var emailAddress: Self { .product(name: "EmailAddress", package: "swift-emailaddress-type") }
-    static var domain: Self { .product(name: "Domain", package: "swift-domain-type") }
     static var asyncHttpClient: Self { .product(name: "AsyncHTTPClient", package: "async-http-client") }
-    static var casePaths: Self { .product(name: "CasePaths", package: "swift-case-paths") }
-    static var dependencies: Self { .product(name: "Dependencies", package: "swift-dependencies") }
     static var dependenciesTestSupport: Self { .product(name: "DependenciesTestSupport", package: "swift-dependencies") }
     static var environmentVariables: Self { .product(name: "EnvironmentVariables", package: "swift-environment-variables") }
-    static var foundationExtensions: Self { .product(name: "FoundationExtensions", package: "swift-foundation-extensions") }
-    static var translating: Self { .product(name: "Translating", package: "swift-translating") }
     static var logging: Self { .product(name: "Logging", package: "swift-log") }
     static var throttling: Self { .product(name: "Throttling", package: "swift-throttling") }
-    static var issueReporting: Self { .product(name: "IssueReporting", package: "xctest-dynamic-overlay") }
-    static var urlRouting: Self { .product(name: "URLRouting", package: "swift-url-routing") }
     static var passwordValidation: Self { .product(name: "PasswordValidation", package: "swift-password-validation") }
-    static var urlRoutingTranslating: Self { .product(name: "URLRoutingTranslating", package: "swift-url-routing-translating") }
-    static var urlFormCoding: Self { .product(name: "URLFormCoding", package: "swift-url-form-coding") }
     static var crypto: Self { .product(name: "Crypto", package: "swift-crypto") }
-    static var tagged: Self { .product(name: "Tagged", package: "swift-tagged") }
-    static var dateParsing: Self { .product(name: "DateParsing", package: "swift-date-parsing") }
-    static var unixEpochParsing: Self { .product(name: "UnixEpochParsing", package: "swift-date-parsing") }
     static var jwt: Self { .product(name: "JWT", package: "swift-jwt") }
 }
 
@@ -56,23 +43,12 @@ let package = Package(
         .library(name: .serverEnvVars, targets: [.serverEnvVars])
     ],
     dependencies: [
-        .package(url: "https://github.com/coenttb/swift-builders", from: "0.0.1"),
-        .package(url: "https://github.com/coenttb/swift-date-parsing", from: "0.1.0"),
-        .package(url: "https://github.com/coenttb/swift-domain-type", from: "0.0.1"),
-        .package(url: "https://github.com/coenttb/swift-emailaddress-type", from: "0.0.1"),
+        .package(url: "https://github.com/coenttb/swift-types-foundation", from: "0.0.1"),
         .package(url: "https://github.com/coenttb/swift-environment-variables", from: "0.0.1"),
-        .package(url: "https://github.com/coenttb/swift-foundation-extensions", from: "0.1.0"),
         .package(url: "https://github.com/coenttb/swift-jwt", from: "0.0.1"),
         .package(url: "https://github.com/coenttb/swift-password-validation", from: "0.0.1"),
         .package(url: "https://github.com/coenttb/swift-throttling", from: "0.0.1"),
-        .package(url: "https://github.com/coenttb/swift-translating", from: "0.0.1"),
-        .package(url: "https://github.com/coenttb/swift-url-form-coding", from: "0.0.1"),
-        .package(url: "https://github.com/coenttb/swift-url-routing-translating", from: "0.0.1"),
-        .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.5.6"),
-        .package(url: "https://github.com/pointfreeco/swift-tagged", from: "0.10.0"),
         .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.9.2"),
-        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.4.3"),
-        .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.6.2"),
         .package(url: "https://github.com/swift-server/async-http-client", from: "1.26.1"),
         .package(url: "https://github.com/apple/swift-log", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-crypto", from: "3.0.0"),
@@ -83,30 +59,17 @@ let package = Package(
         .target(
             name: .serverFoundation,
             dependencies: [
-                .builders,
+                .typesFoundation,
                 .serverEnvVars,
                 .asyncHttpClient,
-                .casePaths,
-                .dependencies,
                 .environmentVariables,
-                .foundationExtensions,
-                .translating,
                 .logging,
                 .loggingExtras,
                 .throttling,
-                .issueReporting,
-                .urlRouting,
                 .urlRequestHandler,
                 .passwordValidation,
-                .urlRoutingTranslating,
-                .emailAddress,
-                .domain,
-                .dateParsing,
-                .unixEpochParsing,
                 .jwt,
                 .crypto,
-                .urlFormCoding,
-                .tagged
             ]
         ),
         .testTarget(
--- Sources/ServerFoundation/exports.swift
+++ Sources/ServerFoundation/exports.swift
@@ -5,18 +5,11 @@
 //  Created by Coen ten Thije Boonkkamp on 21/12/2024.
 //
 
+@_exported import TypesFoundation
 @_exported import AsyncHTTPClient
 @_exported import BoundedCache
-@_exported import Builders
-@_exported import CasePaths
 @_exported import Crypto
-@_exported import DateParsing
-@_exported import Dependencies
-@_exported import Domain
-@_exported import EmailAddress
 @_exported import EnvironmentVariables
-@_exported import Foundation
-@_exported import FoundationExtensions
 @_exported import IssueReporting
 @_exported import JWT
 @_exported import Logging
@@ -24,10 +17,6 @@
 @_exported import PasswordValidation
 @_exported import Throttling
 @_exported import ServerFoundationEnvVars
-@_exported import Translating
-@_exported import URLRouting
-@_exported import URLRoutingTranslating
-@_exported import URLFormCoding
 @_exported import URLRequestHandler
 @_exported import Tagged
 
